85f682c97ddd74e28f2b3420050c800da1a3650b
Yury-Soika <puma10009@gmail.com>
2023-02-25 14:17:21 +0100

    Abstract factory
    Abstract factory solution by Yury Soika

--- AbstractFactory/exercise/shopping-cart-framework.ts
+++ AbstractFactory/exercise/shopping-cart-framework.ts
@@ -1,34 +1,84 @@
-export class Shop {
-    constructor(protected shopFactory: ShopFactoryInterface) {}
-    listProductsWithShippingCost(codes: string[], shippingMethodName: string, miles: number) {
-        return codes.reduce((output, code) => {
-            const product = this.shopFactory.createProduct(code);
-            const shippingMethod = this.shopFactory.createShippingMethod(shippingMethodName);
+import {
+  ProductData,
+  ProductInterface,
+  ShippingMethodData,
+  ShippingMethodInterface,
+  ShopFactoryInterface,
+} from "./shopping-cart-framework";
 
-            return `${output}${output && '\n'}` +
-                `${product.productCode} - ` +
-                `${product.productDescription} / via: ` +
-                `${shippingMethod.courierName}, cost: ` +
-                `\$${shippingMethod.getCostEstimate(miles, product)}`;
-        }, '');
-    }
+export class MyShopProductFactory implements ShopFactoryInterface {
+  #productData: ProductData;
+  #shippingMethodData: ShippingMethodData;
+  constructor(
+    productData: ProductData,
+    shippingMethodData: ShippingMethodData
+  ) {
+    this.#productData = productData;
+    this.#shippingMethodData = shippingMethodData;
+  }
+  createProduct(productCode: keyof ProductData): ProductInterface {
+    return new MyShopProduct(
+      productCode,
+      this.#productData[productCode][0],
+      this.#productData[productCode][1]
+    );
+  }
+  createShippingMethod(
+    shippingName: keyof ShippingMethodData
+  ): ShippingMethodInterface {
+    return new MyShippingMethod(
+      shippingName,
+      this.#shippingMethodData[shippingName][0],
+      this.#shippingMethodData[shippingName][1]
+    );
+  }
 }
 
-export interface ProductData {
-    [id: string]: [string, number]
+export class MyShopProduct implements ProductInterface {
+  #productCode: keyof ProductData;
+  #productDescription: ProductData[keyof ProductData][0];
+  #productWeight: ProductData[keyof ProductData][1];
+
+  constructor(
+    productCode: keyof ProductData,
+    productDescription: ProductData[keyof ProductData][0],
+    productWeight: ProductData[keyof ProductData][1]
+  ) {
+    this.#productCode = productCode;
+    this.#productDescription = productDescription;
+    this.#productWeight = productWeight;
+  }
+  get productCode(): keyof ProductData {
+    return this.#productCode;
+  }
+  get productDescription(): ProductData[keyof ProductData][0] {
+    return this.#productDescription;
+  }
+  get productWeight(): ProductData[keyof ProductData][1] {
+    return this.#productWeight;
+  }
 }
-export interface ShippingMethodData {
-    [name: string]: [number, number]
-}
-export interface ShopFactoryInterface {
-    createProduct(productCode: keyof ProductData): ProductInterface;
-    createShippingMethod(name: keyof ShippingMethodData): ShippingMethodInterface;
-}
-export interface ProductInterface {
-    get productCode(): keyof ProductData;
-    get productDescription(): ProductData[keyof ProductData][0];
-}
-export interface ShippingMethodInterface {
-    get courierName(): keyof ShippingMethodData;
-    getCostEstimate(miles: number, product: ProductInterface): number;
+
+export class MyShippingMethod implements ShippingMethodInterface {
+  #shippingName: keyof ShippingMethodData;
+  #shippingCoefficient1: ShippingMethodData[keyof ShippingMethodData][0];
+  #shippingCoefficient2: ShippingMethodData[keyof ShippingMethodData][1];
+  constructor(
+    shippingName: keyof ShippingMethodData,
+    shippingCoefficient1: ShippingMethodData[keyof ShippingMethodData][0],
+    shippingCoefficient2: ShippingMethodData[keyof ShippingMethodData][1]
+  ) {
+    this.#shippingName = shippingName;
+    this.#shippingCoefficient1 = shippingCoefficient1;
+    this.#shippingCoefficient2 = shippingCoefficient2;
+  }
+  get courierName(): keyof ShippingMethodData {
+    return this.#shippingName;
+  }
+  getCostEstimate(miles: number, product: ProductInterface): number {
+    return (
+      miles * this.#shippingCoefficient1 +
+      product.productWeight * this.#shippingCoefficient2
+    );
+  }
 }
